29cb36e8bbb7aab243bcfbebffea5cfde807078c
Tanush Chauhan <tanushchauhan07@gmail.com>
2024-08-15 20:11:15 -0500

    Completed the main game

--- encrypt_snake_game.py
+++ encrypt_snake_game.py
@@ -92,93 +92,59 @@ def make_short(bin_str):
     return ''.join(z_2)
 
 def make_long(enc_text):
-    if enc_text[0] == "-" and enc_text[1] == "-" and enc_text[2] == "9" and enc_text[len(enc_text)-3] == "9" and enc_text[len(enc_text)-2] == "-" and enc_text[len(enc_text)-1] == "-":
-        enc_text_list = []
-        for i  in enc_text:
-            enc_text_list.append(i)
-        enc_text_list[0] = ""
-        enc_text_list[1] = ""
-        enc_text_list[2] = ""
-        enc_text_list[len(enc_text)-3] = ""
-        enc_text_list[len(enc_text)-2] = ""
-        enc_text_list[len(enc_text)-1] = ""
-        z_2 = enc_text_list
-        indices_ab = [i for i, x in enumerate(enc_text_list) if x == "q"]
-        for i in indices_ab:
-            try:
-                z_2[i] = "ab"
-            except:
-                pass
-        indices_ac = [i for i, x in enumerate(enc_text_list) if x == "w"]
-        for i in indices_ac:
-            try:
-                z_2[i] = "ac"
-            except:
-                pass
-        indices_ad = [i for i, x in enumerate(enc_text_list) if x == "!"]
-        for i in indices_ad:
-            try:
-                z_2[i] = "ad"
-            except:
-                pass
-        indices_ba = [i for i, x in enumerate(enc_text_list) if x == "e"]
-        for i in indices_ba:
-            try:
-                z_2[i] = "ba"
-            except:
-                pass 
-        indices_bc = [i for i, x in enumerate(enc_text_list) if x == "r"]
-        for i in indices_bc:
-            try:
-                z_2[i] = "bc"
-            except:
-                pass
-        indices_bd = [i for i, x in enumerate(enc_text_list) if x == "g"]
-        for i in indices_bd:
-            try:
-                z_2[i] = "bd"
-            except:
-                pass
-        indices_ca = [i for i, x in enumerate(enc_text_list) if x == "t"]
-        for i in indices_ca:
-            try:
-                z_2[i] = "ca"
-            except:
-                pass
-        indices_cb = [i for i, x in enumerate(enc_text_list) if x == "%"]
-        for i in indices_cb:
-            try:
-                z_2[i] = "cb"
-            except:
-                pass
-        indices_cd = [i for i, x in enumerate(enc_text_list) if x == "&"]
-        for i in indices_cd:
-            try:
-                z_2[i] = "cd"
-            except:
-                pass
-        indices_aa = [i for i, x in enumerate(enc_text_list) if x == "@"]
-        for i in indices_aa:
-            try:
-                z_2[i] = "aa"
-            except:
-                pass
-        indices_bb = [i for i, x in enumerate(enc_text_list) if x == "#"]
-        for i in indices_bb:
-            try:
-                z_2[i] = "bb"
-            except:
-                pass
-        indices_cc = [i for i, x in enumerate(enc_text_list) if x == "o"]
-        for i in indices_cc:
-            try:
-                z_2[i] = "cc"
-            except:
-                pass
-        indices_dc = [i for i, x in enumerate(enc_text_list) if x == "j"]
-        for i in indices_dc:
-            try:
-                z_2[i] = "dc"
-            except:
-                pass
-        indices_db = [i for i, x in enumerate(enc_text_list) if x == "k"]
+    # Check for valid start and end markers
+    if enc_text.startswith("--9") and enc_text.endswith("9--"):
+        # Strip off the identity markers
+        enc_text = enc_text[3:-3]
+        
+        # Dictionary for first round of replacements
+        replacements_1 = {
+            "q": "ab", "w": "ac", "!": "ad", "e": "ba", "r": "bc", "g": "bd",
+            "t": "ca", "%": "cb", "&": "cd", "@": "aa", "#": "bb", "o": "cc",
+            "j": "dc", "k": "db", "l": "da"
+        }
+        
+        # Perform first round of replacements
+        z_2 = [replacements_1.get(char, char) for char in enc_text]
+        
+        # Join the result for the next processing step
+        z_2_string = ''.join(z_2)
+        
+        # Dictionary for second round of replacements
+        replacements_2 = {
+            "a": "10", "b": "01", "c": "11", "d": "00"
+        }
+        
+        # Perform second round of replacements
+        z = [replacements_2.get(char, char) for char in z_2_string]
+        
+        # Return the final reconstructed binary string
+        return ''.join(z)
+    else:
+        return False
+
+def decrypt(bin):
+    c = 0
+    temp = ""
+    z = []
+    for i in bin:
+        c += 1
+        if len(temp) == 8:
+            z.append(temp)
+            temp = ""
+            temp += str(i)
+        else:
+            temp += str(i)
+            if len(temp) == 8:
+                z.append(temp)
+                temp = ""
+    decrypted_text_in_binary_list = []
+    for i in z:
+        user_final = i
+        for i in range(1,9):
+            y = key.index(i)
+            decrypted_text_in_binary_list.append(user_final[y])
+    decrypted_text_in_binary = ""
+    for i in decrypted_text_in_binary_list:
+        decrypted_text_in_binary += i
+    return binary_to_text(decrypted_text_in_binary)
